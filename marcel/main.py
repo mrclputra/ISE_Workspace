d5b657f4cbbf246bbb55919f865ce4cf115ba895
Marcel Putra <marcelino.ares.putra@gmail.com>
2024-07-25 15:59:32 +0800

    implement array image loading

--- marcel/main.py
+++ marcel/main.py
@@ -1,19 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from skimage import io, exposure, img_as_float
+from skimage import io, img_as_float
+
+# this implements skimage, array loading approach
+# need to convert to independent variables for images so that we can implement editing
+# this is a stopgap for testing
 
 # set window dimensions to 1080p (optional)
 dpi = 100
 width_inches = 1920 / dpi
 height_inches = 1080 / dpi
 
-# define where image components are stored
+# get where image components are stored
 components_dir = 'marcel/components'
 png_files = [f for f in os.listdir(components_dir) if f.endswith('.png')]
 
+# here you can manually choose what files to load
+# array order dictates layer order
+manual_order = [
+    'base_sky.png',
+    'sun_redgiant.png',
+    'clouds_sun.png',
+    'fuji_base.png',
+    'base_ground.png',
+    'clouds_fuji.png',
+    'clouds_close.png',
+    'car_base.png',
+    'subject_shadows.png',
+    'time_traveller.png'
+]
+
+ordered_files = [file for file in manual_order if file in png_files]
+
+# load images in array
 images = []
-for file in png_files:
+for file in ordered_files:
     image_path = os.path.join(components_dir, file)
     image = img_as_float(io.imread(image_path))
     images.append(image)
